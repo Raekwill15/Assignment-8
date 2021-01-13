e713481208e10ce4a774d8194673cbc43126cfc2
Daniel <dhonaruto@gmail.com>
2021-01-12 19:10:48 -0500

    Implemented buttons and functions in Table.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import Table from './components/Table';
+import Reac, {Component} from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Table/>
     </div>
+
   );
 }
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -10,9 +10,37 @@ class Table extends Components{
             numColumn: 0,
             tableArray: []
         }
-        <TableRow/>
+
+    this.changeColor = this.changeColor.bind(this);
+    this.addRow = this.addRow.bind(this);
+    this.addColumn = this.addColumn.bind(this);
+    }
+
+    changeColor(evt){
+        this.setState({currentColor: evt.target.value});
+    }
+
+    addRow = () =>{
+
+    }
+
+    addColumn = () =>{
         
     }
 
+    render(){
+        return( 
+            <div className="table">
+                <button className="addRow" onClick={this.addRow}>Add Row</button>
+                <button className="addColumn" onClick={this.addColumn}>Add Column</button>
+                <select name="color" onChange={this.changeColor}>
+                    <option>Blue</option>
+                    <option>Red</option>
+                    <option>Green</option>
+                    <option>Purple</option>
+                </select>
+            </div>
+        );
+    }
 }
 export default Table;
